fc3ec5436336b7b09dedfb7e2afee98769c502a8
Markéta Musílková <mrs.musilkova@gmail.com>
2024-04-28 22:01:02 +0300

    Úprava záhlaví a zápatí na stránce detailu objednávky

--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -2,20 +2,13 @@ import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
 import './order.css';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <div className="page">
-      <header>
-        <div className="header__content container">
-          <div className="site-logo"></div>
-
-          <nav className="inline-nav">
-            <a href="/">Hlavní stránka</a>
-          </nav>
-
-        </div>
-      </header>
+     <Header showMenu={false}/>
 
       <main className="order">
         <div className="order__content container">
@@ -45,13 +38,7 @@ document.querySelector('#root').innerHTML = render(
         </div>
       </main>
 
-      <footer>
-        <div className="container">
-          <div className="footer__content">
-            Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   </div>
 );
